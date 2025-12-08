e8ad99ef71fecd4588beb030bb9da80aa0b6e2c5
Vasilca Rares <160517053+vasilca-rares-mihai@users.noreply.github.com>
2025-12-08 06:36:20 -0800

    Update challenge.py

--- src/utils/challenge.py
+++ src/utils/challenge.py
@@ -5,8 +5,4 @@ ANALYZERS = {
     'treadmill': StepAnalyzer,
     'pullup': PullupAnalyzer,
     'vjump': VerticalJumpAnalyzer,
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> b55b29063857dcab8f50645ce599aa4fdd7d0260
