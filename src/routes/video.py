8fc1fe0a1250bab3f963a18834dfc6ca33a65cd9
Vasilca Rares-Mihai <rares-mihai.vasilca@student.tuiasi.ro>
2025-12-23 12:27:17 +0200

    reorganization of directories

---
+++ src/routes/video.py
@@ -0,0 +1,4 @@
+from fastapi import FastAPI, APIRouter
+
+app = FastAPI()
+router = APIRouter(prefix="/api/video", tags=["Video"])
---
+++ src/utils/enums.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+
+class GenderEnum(str, Enum):
+    Male = 'Male'
+    Female = 'Female'
+    Other = 'Other'
+
+class PositionsEnum(str, Enum):
+    goalkeeper = 'goalkeeper'
+    center_back = "center_back"
+    full_back = "full_back"
+    defensive_midfielder = "defensive_midfielder"
+    midfielder = "midfielder"
+    attacking_midfielder  = "attacking_midfielder"
+    winger = "winger"
+    attacker = "attacker"
+
+class WeakFootEnum(str, Enum):
+    left = 'left'
+    right = 'right'
