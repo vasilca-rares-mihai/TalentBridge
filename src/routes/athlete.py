6299a40a493e0da0661839255ca4eba94066aed3
Vasilca Rares-Mihai <rares-mihai.vasilca@student.tuiasi.ro>
2025-12-23 18:06:39 +0200

    GET route to search an athlete using filters (field position, age, gender, waek foot, height, weight, country)

--- src/routes/athlete.py
+++ src/routes/athlete.py
@@ -3,10 +3,12 @@ from fastapi import FastAPI, APIRouter, Depends
 from typing import List
 from schemas.schemas import AthleteBase
 from crud import data_access
+from utils.enums import GenderEnum, PositionsEnum, WeakFootEnum
 from core.database import get_db
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 from fastapi import HTTPException, status
+from typing import Optional
 
 app = FastAPI()
 router = APIRouter(prefix="/api/athlete", tags=["Athlete"])
@@ -62,4 +64,23 @@ def delete_athlete(athlete_id: int, db: Session = Depends(get_db)):
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Athlete delete error (api.py)"
+        )
+
+#search athlete by filter
+@router.get("/athletes/search", response_model=List[AthleteBase], summary= "*FOOTBALL CLUB ONLY* Search athlete by filters")
+def search_athletes(db: Session = Depends(get_db), field_position: Optional[PositionsEnum] = None, age: Optional[int] = None, gender: Optional[GenderEnum] = None, weak_foot: Optional[WeakFootEnum] = None, height: Optional[float] = None, weight: Optional[float] = None, country: Optional[str] = None):
+
+    try:
+        athletes = data_access.list_athletes_by_filter(db, field_position, age, gender, weak_foot, height, weight, country)
+        if athletes is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="athletes not found"
+            )
+        return athletes
+    except HTTPException as http_ex:
+        raise http_ex
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
         )
