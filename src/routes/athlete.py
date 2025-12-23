be040957e0cecdccee1a7a61442e97f71ce17527
Vasilca Rares-Mihai <rares-mihai.vasilca@student.tuiasi.ro>
2025-12-23 17:52:27 +0200

    POST route to insert into athlete and attribute tables

--- src/routes/athlete.py
+++ src/routes/athlete.py
@@ -5,6 +5,7 @@ from schemas.schemas import AthleteBase
 from crud import data_access
 from core.database import get_db
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 from fastapi import HTTPException, status
 
 app = FastAPI()
@@ -23,3 +24,21 @@ def get_athletes(db: Session = Depends(get_db)):
         )
 
 
+@router.post("/athletes", response_model=AthleteBase, summary="Create a new athlete")
+def insert_athlete_db(athlete_data: AthleteBase, db: Session = Depends(get_db)):
+    try:
+        athlete = data_access.create_athlete(db, athlete_data)
+        return athlete
+    except IntegrityError as e:
+        db.rollback()
+        print(f"error: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="phone number or email already exists"
+        )
+    except Exception as e:
+        print(e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Athlete create error (api.py)"
+        )
