15606982e1987970d7e01e123e8b4cd2cd70d2ae
Vasilca Rares-Mihai <rares-mihai.vasilca@student.tuiasi.ro>
2025-12-23 21:08:28 +0200

    POST route to analyze a video

---
+++ src/analyzers/__init__.py
@@ -0,0 +1,7 @@
+from .squat import SquatAnalyzer
+from .pushup import PushupAnalyzer
+from .treadmill import StepAnalyzer
+from .pullup import PullupAnalyzer
+from .vertical_jump import VerticalJumpAnalyzer
+from .base import VideoAnalyzer
+from .base import mp_pose
