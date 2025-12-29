52870c6dbf462dde9553a3817957caaf915cf3d1
Vasilca Rares-Mihai <rares-mihai.vasilca@student.tuiasi.ro>
2025-12-29 13:44:24 +0200

    vf jwt token

--- src/main.py
+++ src/main.py
@@ -1,10 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, status
 from routes import athlete, attribute, challenge, video, user
+from crud.security import security
 
 app = FastAPI(title="API TalentBridge")
+
+@app.get("/protected-resource", dependencies=[Depends(security)], status_code=status.HTTP_200_OK)
+def protected_router():
+    return {"message": "true"}
+
 
 app.include_router(athlete.router)
 app.include_router(attribute.router)
 app.include_router(video.router)
 app.include_router(challenge.router)
 app.include_router(user.router)
+
+
+#uvicorn main:app --reload
