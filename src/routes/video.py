802c2b7f446e2491de9e6ef79ec6e88a36b3db53
Vasilca Rares-Mihai <rares-mihai.vasilca@student.tuiasi.ro>
2025-12-29 16:47:39 +0200

    video routes updated (checking permissions)

--- src/routes/video.py
+++ src/routes/video.py
@@ -1,3 +1,4 @@
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
 from crud import data_access
 from core.database import get_db
@@ -6,12 +7,20 @@ from routes.challenge import insert_challenge_result_db
 from utils.challenge import ANALYZERS
 import os
 import shutil
+from crud.security import get_current_user
 
 app = FastAPI()
 router = APIRouter(prefix="/api/video", tags=["Video"])
 
-@router.post("/upload-video/", summary="Upload video")
-async def upload_video(athlete_id: int, id_challenge : int, db: Session = Depends(get_db), file: UploadFile = File(...)):
+@router.post("/upload-video/", summary="*ATHLETE ONLY* Upload video")
+async def upload_video(athlete_id: int, id_challenge : int, db: Session = Depends(get_db), file: UploadFile = File(...), current_user: dict = Depends(get_current_user)):
+
+    athlete = data_access.get_athlete_by_id(db, athlete_id)
+    if current_user.get("role") != "athlete" and current_user.get("email") != athlete.email:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You don t have access to upload a video."
+        )
     athlete = data_access.get_athlete_by_id(db, athlete_id)
     workout_type = data_access.get_challenge_by_id(db, id_challenge).challenge_name
     save_directory = os.path.join("videos", workout_type, str(athlete.id_athlete))
@@ -28,15 +37,33 @@ async def upload_video(athlete_id: int, id_challenge : int, db: Session = Depend
             "saved_path": video_path,
             "filename": file.filename
         }
-
-    except Exception as e:
+    except SQLAlchemyError as e:
+        db.rollback()
+        print(f"Database error: {e}")
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="database error"
+        )
+    except HTTPException as e:
+        db.rollback()
+        return e
+    except Exception as e:
+        db.rollback()
+        print(f"Unexpected error: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Unexpected error"
         )
 
-@router.post("/analyze", summary="Run analysis on an uploaded video")
-def run_analysis_route(id_athlete: int, id_challenge: int, db: Session = Depends(get_db)):
+@router.post("/analyze", summary="*ATHLETE ONLY* Run analysis on an uploaded video")
+def run_analysis_route(id_athlete: int, id_challenge: int, db: Session = Depends(get_db), current_user: dict = Depends(get_current_user)):
     athlete = data_access.get_athlete_by_id(db, id_athlete)
+    print(current_user.get("email"), athlete.email)
+    if current_user.get("email") != athlete.email:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You don t have access to analyze a video.",
+        )
     workout_type = data_access.get_challenge_by_id(db, id_challenge).challenge_name
     save_directory = os.path.join("videos", workout_type, str(athlete.id_athlete))
     os.makedirs(save_directory, exist_ok=True)
@@ -69,11 +96,22 @@ def run_analysis_route(id_athlete: int, id_challenge: int, db: Session = Depends
         if os.path.exists(video_path):
             os.remove(video_path)
         #def insert_challenge_result_db(id_challenge: int, id_athlete: int, result: int, db: Session = Depends(get_db)):
-        insert_challenge_result_db(id_challenge, athlete.id_athlete, analysis_result, db)
+        insert_challenge_result_db(id_challenge, athlete.id_athlete, analysis_result, db, current_user)
 
-    except Exception as e:
-        print(e)
+    except SQLAlchemyError as e:
+        db.rollback()
+        print(f"Database error: {e}")
         raise HTTPException(
-            status_code=400,
-            detail=f"Error analyzing {video_path}: {e}"
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="database error"
+        )
+    except HTTPException as e:
+        db.rollback()
+        raise e
+    except Exception as e:
+        db.rollback()
+        print(f"Unexpected error: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Unexpected error"
         )
