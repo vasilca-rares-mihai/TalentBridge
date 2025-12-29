b0bbb03756ce86f838ad0ef52ad3fe1391d72060
Vasilca Rares-Mihai <rares-mihai.vasilca@student.tuiasi.ro>
2025-12-29 12:49:30 +0200

    update to this route, try catch errors

--- src/routes/user.py
+++ src/routes/user.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI, Depends, APIRouter
+from fastapi import FastAPI, Depends, APIRouter, HTTPException
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
+from starlette import status
+
 from crud import data_access
 from core.database import get_db
 from sqlalchemy.orm import Session
@@ -12,8 +15,28 @@ router = APIRouter(prefix="/api/users", tags=["Users"])
 
 @router.post("/user", summary="Create a user-athlete account")
 def create_user_account(athlete_data: AthleteBase, email: str, password: str, db: Session = Depends(get_db)):
-    role = RolesEnum.athlete
-    password_hash = hash_password(password)
-    user = data_access.create_user(db, email, password_hash, role)
-    insert_athlete_db(athlete_data, email, db)
-    return user
+    try:
+        role = RolesEnum.athlete
+        password_hash = hash_password(password)
+        user = data_access.create_user(db, email, password_hash, role)
+        insert_athlete_db(athlete_data, email, db)
+        return user
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="An account with this email already exists"
+        )
+    except SQLAlchemyError as e:
+        db.rollback()
+        print(f"Database Error: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An error occurred while saving data to the database"
+        )
+    except Exception as e:
+        print(f"Unexpected Error: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal server error"
+        )
