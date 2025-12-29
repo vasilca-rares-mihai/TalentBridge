8b89e3901bb303ac5eeb8b947418d6244c6efaaa
Vasilca Rares-Mihai <rares-mihai.vasilca@student.tuiasi.ro>
2025-12-29 12:38:59 +0200

    POST routes to create admin and football club accounts. work in progress, football club account route is not finished

--- src/routes/user.py
+++ src/routes/user.py
@@ -17,3 +17,17 @@ def create_user_account(athlete_data: AthleteBase, email: str, password: str, db
     user = data_access.create_user(db, email, password_hash, role)
     insert_athlete_db(athlete_data, email, db)
     return user
+
+@router.post("/admin", summary="Create an admin account")
+def create_admin_account(email: str, password: str, db: Session = Depends(get_db)):
+    role = RolesEnum.admin
+    password_hash = hash_password(password)
+    admin = data_access.create_user(db, email, password_hash, role)
+    return admin
+
+@router.post("/football_club", summary="Create a football club account")
+def create_football_club_account(email: str, password: str, db: Session = Depends(get_db)):
+    role = RolesEnum.football_club
+    password_hash = hash_password(password)
+    football_club = data_access.create_user(db, email, password_hash, role)
+    return football_club
